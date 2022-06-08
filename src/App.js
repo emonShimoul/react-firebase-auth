e48effd15d5eb9a8f2b8646a5bcc6fa3edeb910b
emonShimoul <emonshimoul@gmail.com>
2022-06-08 12:46:01 +0600

    toggle registration and login, implement sign in with email password

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
 import initializeAuthentication from './Firebase/firebase.init';
 
@@ -9,6 +9,7 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
   const auth = getAuth();
 
   const handleRegistration = e => {
@@ -22,6 +23,35 @@ function App() {
       setError('Password must contain 2 uppercase!!');
       return;
     }
+
+    isLogin ? processLogin(email, password) : registerNewUser(email, password);
+
+  }
+
+  const processLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      setError(error.message);
+    })
+  }
+
+  const toggleLogin = e => {
+    setIsLogin(e.target.checked);
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  }
+
+  const registerNewUser = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
     .then(result => {
       const user = result.user;
@@ -33,18 +63,10 @@ function App() {
     })
   }
 
-  const handleEmailChange = e => {
-    setEmail(e.target.value);
-  }
-
-  const handlePasswordChange = e => {
-    setPassword(e.target.value);
-  }
-
   return (
     <div className="mx-5 mt-5">
       <form onSubmit={handleRegistration}>
-        <h3 className='text-primary mb-4'>Please Register:</h3>
+        <h3 className='text-primary mb-4'>Please {isLogin ? 'Login' : 'Register'}:</h3>
         <div className="row mb-3">
           <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
           <div className="col-sm-10">
@@ -60,15 +82,17 @@ function App() {
         <div className="row mb-3">
           <div className="col-sm-10 offset-sm-2">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
               <label className="form-check-label" htmlFor="gridCheck1">
-                Example checkbox
+                Already Registered??
               </label>
             </div>
           </div>
         </div>
         <div className="row mb-3 text-danger">{error}</div>
-        <button type="submit" className="btn btn-primary">Register</button>
+        <button type="submit" className="btn btn-primary">
+          {isLogin ? 'Login' : 'Register'}
+        </button>
       </form>
     </div>
   );
