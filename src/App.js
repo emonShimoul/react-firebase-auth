8470e3563a372e86916c09a135c00d7e984d791f
emonShimoul <emonshimoul@gmail.com>
2022-06-08 11:18:47 +0600

    create simple login form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleRegistration = e => {
+    console.log('Registration will be added');
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleRegistration}>
+        <h3>Please Register</h3>
+        <label htmlFor="email">Email: </label>
+        <input type="text" name="email" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="text" name='password' id="" />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 }
