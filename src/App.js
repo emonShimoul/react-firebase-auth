80bb87ad50f52878edb20ede0287bfce08b4602f
emonShimoul <emonshimoul@gmail.com>
2022-06-08 11:34:28 +0600

    get user email and password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleRegistration = e => {
-    console.log('Registration will be added');
+    console.log(email, password);
     e.preventDefault();
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
   }
 
   return (
@@ -15,13 +24,13 @@ function App() {
         <div class="row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
           <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" />
+            <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3" />
           </div>
         </div>
         <div class="row mb-3">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" />
+            <input onBlur={handlePasswordChange} type="password" class="form-control" id="inputPassword3" />
           </div>
         </div>
         <div class="row mb-3">
