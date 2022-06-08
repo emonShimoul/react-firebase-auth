7bc6cfa44e5c236e319966618302164779cd6dfe
emonShimoul <emonshimoul@gmail.com>
2022-06-08 12:03:31 +0600

    create new user using email and password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
+import initializeAuthentication from './Firebase/firebase.init';
 
-function App() {
+initializeAuthentication();
+
+function App() {  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const auth = getAuth();
+
   const handleRegistration = e => {
     console.log(email, password);
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
     e.preventDefault();
   }
 
@@ -21,29 +32,29 @@ function App() {
     <div className="mx-5 mt-5">
       <form onSubmit={handleRegistration}>
         <h3 className='text-primary mb-4'>Please Register:</h3>
-        <div class="row mb-3">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3" />
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required/>
           </div>
         </div>
-        <div class="row mb-3">
-          <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-          <div class="col-sm-10">
-            <input onBlur={handlePasswordChange} type="password" class="form-control" id="inputPassword3" />
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" required/>
           </div>
         </div>
-        <div class="row mb-3">
-          <div class="col-sm-10 offset-sm-2">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck1" />
-              <label class="form-check-label" for="gridCheck1">
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" htmlFor="gridCheck1">
                 Example checkbox
               </label>
             </div>
           </div>
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" className="btn btn-primary">Register</button>
       </form>
     </div>
   );
